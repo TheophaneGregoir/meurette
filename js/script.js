ceccf44ca66154405cd94b62bac8d07aa81f1c51
TheoGreg <theophane.gregoir13@gmail.com>
2025-07-18 18:54:23 +0200

    first working mobile navigation

--- js/script.js
+++ js/script.js
@@ -11,13 +11,14 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
-// // Make mobile navigation work
-// const btnNavEl = document.querySelector(".btn-mobile-nav");
-// const headerEl = document.querySelector(".header");
+// Make mobile navigation work
 
-// btnNavEl.addEventListener("click", function () {
-//   headerEl.classList.toggle("nav-open");
-// });
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
